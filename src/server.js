bb431c9eaa148bfa5bee4dc6e825a9bd1eb25e1d
anaesparapani <anaclaraesparapani@gmail.com>
2024-09-18 16:02:03 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,8 @@
+const app = require("./index");
+
+//Inicia o servidor na porta 5000, neste caso como está local a
+//Api será acessível em http://localhost:5000/
+
+// http://localhost:5000/agen/sala-de-aula/v1/health
+
+app.listen(5000);
