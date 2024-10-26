dac65c88b5c8b586b2e34320dd1eca7ed3d545d8
anaesparapani <159201364+anaesparapani@users.noreply.github.com>
2024-10-26 15:30:23 -0300

    connect.js

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');//Importa o mysql
 
-const pool = mysql.createPool({
+const pool = mysql.createPool({ //metodo para criar conexões
     connectionLimit:10,
     host:'localhost',
     user:'alunods',
     password:'senai@604',
-    database:'vio_reservas'
+    database:'vio_reservas' //nome do banco que está fazendo a conexão
 });
 
 module.exports = pool;//Para usar o mysql
